1067eed58d18a9c338beb733ec6cab1c2f521629
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2020-12-04 15:03:32 +0100

    Build: Load saved cache when initializing new LavaStore

--- lib/LavaStore.d.ts
+++ lib/LavaStore.d.ts
@@ -3,5 +3,6 @@ import { IDictionary } from './IDictionary';
 import { LSCollection } from './LSCollection';
 export declare class LavaStore extends LSDocument {
     set parent(p: undefined);
+    constructor(id: string, fields?: object, collections?: IDictionary<LSCollection>);
 }
 export { IDictionary, LSDocument, LSCollection };
