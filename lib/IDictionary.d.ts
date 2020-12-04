e02c607dbab9d5f40978ff421ba135f866a0dc45
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2020-12-04 14:33:45 +0100

    Combine all files into LavaStore

--- lib/IDictionary.d.ts
+++
@@ -1,3 +0,0 @@
-export interface IDictionary<TValue> {
-    [id: string]: TValue;
-}
--- lib/LSWrapper.d.ts
+++
@@ -1,7 +0,0 @@
-declare abstract class LSWrapper {
-    private static serialize;
-    private static deserialize;
-    static Save: (label: string, data: any) => void;
-    static Load: (label: string) => any;
-    static Contains: (label: string) => boolean;
-}
