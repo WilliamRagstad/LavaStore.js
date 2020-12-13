27d647f7963b99a85483a2cce13f3c95c47eef43
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2020-12-13 02:06:59 +0100

    Added LSHelpers

--- lib/LavaStore.d.ts
+++ lib/LavaStore.d.ts
@@ -1,9 +1,10 @@
 import { LSDocument } from "./LSDocument";
 import { IDictionary } from './IDictionary';
 import { LSCollection } from './LSCollection';
+import LSHelpers from './LSHelpers';
 declare class LavaStore extends LSDocument {
     set parent(p: undefined);
     constructor(id: string, fields?: object, collections?: IDictionary<LSCollection>);
 }
 export default LavaStore;
-export { IDictionary, LSDocument, LSCollection };
+export { IDictionary, LSDocument, LSCollection, LSHelpers };
